f2711fa8828961cc4206624bd89896b6b0370131
Rafael Amorim <contatofael@gmail.com>
2016-10-17 15:48:02 -0200

    updating playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//for index in 2...20 {
+//    if(index % 2 == 0){
+//        print(index)
+//    }
+//}
+
+var i = 1
+
+while i <= 6 {
+    
+    if i > 4 {
+        
+        print(3*i)
+        
+    }
+    
+    i += 1
+    
+}
+
